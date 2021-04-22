bbe3899e96b6cd8788bd1d43e88340702df309c8
Emanuel Mahler <e.mahler@esri.ch>
2021-04-22 16:46:08 +0200

    Fix release build script

--- build/build_release_Quaestor.ps1
+++ build/build_release_Quaestor.ps1
@@ -4,6 +4,6 @@ $version = [Version] $xml.Project.PropertyGroup.Version
 
 $outputDir = ".\output\Quaestor_" + $version
 
-dotnet publish "..\src\Quaestor.Console\Quaestor.Console.csproj" --runtime win-x64 -c Release --self-contained --output @outputDir
+dotnet publish "..\src\Quaestor.Console\Quaestor.Console.csproj" --runtime win-x64 -c Release --self-contained --output $outputDir
 
 pause
