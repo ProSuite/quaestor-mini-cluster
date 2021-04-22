aeca17f02f01631fe0abb9515c05ee3b54122dad
Emanuel Mahler <e.mahler@esri.ch>
2021-04-22 08:15:26 +0200

    Add build scripts

---
+++ build/build_release_Quaestor.ps1
@@ -0,0 +1,9 @@
+
+$xml = [Xml] (Get-Content ..\src\Directory.Build.props)
+$version = [Version] $xml.Project.PropertyGroup.Version
+
+$outputDir = ".\output\Quaestor_" + $version
+
+dotnet publish "..\src\Quaestor.Console\Quaestor.Console.csproj" --runtime win-x64 -c Release --self-contained --output @outputDir
+
+pause
