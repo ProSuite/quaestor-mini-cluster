6faff63404ebb3dd88d3f5fffa8d01ca0e232107
Emanuel Mahler <e.mahler@esri.ch>
2021-05-05 21:06:03 +0200

    Add support for TLS in grpCurl scripts

--- examples/CheckHealth.ps1
+++ examples/CheckHealth.ps1
@@ -4,12 +4,20 @@
 # Make sure grpcurl.exe (https://github.com/fullstorydev/grpcurl) is in the path
 #
 # This example actually works while the unit tests in LoadReportingGrpcImplTest are running. 
-# Otherwise change the address and possibly switch from -plaintext to -cacert
+# 
+# Usage:
+# CheckHealth.ps1 HOSTNAME:Port ServiceName {pem file with certificate authority}
 #
+# Example (insecure):
+# CheckHealth.ps1 localhost:5150 ServiceDiscoveryGrpc
+#
+# Example (using TLS):
+# CheckHealth.ps1 MY_MACHINE.domain.com:5150 ServiceDiscoveryGrpc "C:\Temp\root_ca.cer"
 
 param (
 	$Address = $(throw "Address in the form localhost:5151 is required."),
-	$ServiceName = $(throw "Service name is required.")
+	$ServiceName = $(throw "Service name is required."),
+	$cacert_file
 )
 
 Write-host "Checking service health for $ServiceName at address $Address"
@@ -19,10 +27,21 @@ $dir = Split-Path $scriptpath
 cd $dir
 Write-host "Changed directory to $dir"
 
+if ($cacert_file) 
+{
+	Write-host "TLS: Using specified pem file: " $cacert_file
+	$tlsArg="-cacert"
+}
+else 
+{
+	Write-host "Using insecure credentials (no TLS)"
+	$tlsArg="-plaintext"
+}
+
 $requestJson= '{\"service\": \"' + $ServiceName + '\"}'
 
 Write-host "grpcurl response:"
-grpcurl.exe -d $requestJson -plaintext -import-path .\ -proto health.proto $Address grpc.health.v1.Health/Check
+grpcurl.exe -d $requestJson $tlsArg $cacert_file -import-path .\ -proto health.proto $Address grpc.health.v1.Health/Check
 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
--- examples/DiscoverTopService.ps1
+++ examples/DiscoverTopService.ps1
@@ -4,12 +4,20 @@
 # Make sure grpcurl.exe (https://github.com/fullstorydev/grpcurl) is in the path
 #
 # This example actually works while the unit tests in LoadReportingGrpcImplTest are running. 
-# Otherwise change the address and possibly switch from -plaintext to -cacert
+# 
+# Usage:
+# DiscoverTopService.ps1 HOSTNAME:Port ServiceName {PEM file with certificate authority}
 #
+# Example (insecure):
+# DiscoverTopService.ps1 localhost:5150 QualityVerificationGrpc
+#
+# Example (using TLS):
+# DiscoverTopService.ps1 MY_MACHINE.domain.com:5150 QualityVerificationGrpc "C:\Temp\root_ca.cer"
 
 param (
 	$Address = $(throw "Address in the form localhost:5151 is required."),
-	$ServiceName = $(throw "Service name is required.")
+	$ServiceName = $(throw "Service name is required."),
+	$cacert_file
 )
 
 Write-host "Discovering services $ServiceName at address $Address"
@@ -19,10 +27,21 @@ $dir = Split-Path $scriptpath
 cd $dir
 Write-host "Changed directory to $dir"
 
+if ($cacert_file) 
+{
+	Write-host "TLS: Using specified PEM file: " $cacert_file
+	$tlsArg="-cacert"
+}
+else 
+{
+	Write-host "Using insecure credentials (no TLS)"
+	$tlsArg="-plaintext"
+}
+
 $serviceNameJson= '{\"service_name\": \"' + $ServiceName + '\", \"max_count\": "1"}'
 
 Write-host "grpcurl response:"
-grpcurl.exe -d $serviceNameJson -plaintext -import-path ..\src\Quaestor.ServiceDiscovery -proto service_discovery.proto $Address Quaestor.ServiceDiscovery.ServiceDiscoveryGrpc/DiscoverTopServices
+grpcurl.exe -d $serviceNameJson $tlsArg $cacert_file -import-path ..\src\Quaestor.ServiceDiscovery -proto service_discovery.proto $Address Quaestor.ServiceDiscovery.ServiceDiscoveryGrpc/DiscoverTopServices
 
 Write-Host -NoNewLine 'Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
