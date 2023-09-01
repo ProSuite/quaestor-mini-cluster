4abb69d30ed9220b8b2111cba430897fb1a3559f
Emanuel Mahler <e.mahler@dirageosystems.ch>
2023-09-01 10:19:53 +0200

    Add new executables to build scripts

--- build/build_release_Quaestor.ps1
+++ build/build_release_Quaestor.ps1
@@ -5,6 +5,8 @@ $version = [Version] $xml.Project.PropertyGroup.Version
 $outputDir = ".\output\Quaestor_" + $version
 
 dotnet publish "..\src\Quaestor.Console\Quaestor.Console.csproj" --runtime win-x64 -c Release --no-self-contained --output $outputDir
+dotnet publish "..\src\Quaestor.Cluster.Console\Quaestor.Cluster.Console.csproj" --runtime win-x64 -c Release --no-self-contained --output $outputDir
+dotnet publish "..\src\Quaestor.LoadBalancer.Console\Quaestor.LoadBalancer.Console.csproj" --runtime win-x64 -c Release --no-self-contained --output $outputDir
 
 Copy-Item ..\LICENSE -Destination .\output
 Copy-Item ..\README.md -Destination .\output
